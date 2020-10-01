c4a8507e60f15e83c1b7cc3c17a4109e852eeceb
Hishikesh rathor <58757878+Hrishi2322@users.noreply.github.com>
2020-10-01 17:44:03 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b;
+    printf("Enter the value of a and b");
+    scanf("%d %d",&a,&b);
+    printf("Sum of two Values is %d",a+b);
+    return 0;
+}
